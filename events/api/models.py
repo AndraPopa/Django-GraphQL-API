3f4673521528912940de525d061c9ffaa26886f1
AndraPopa <93611644+AndraPopa@users.noreply.github.com>
2024-04-08 10:37:08 +0300

    added presenter

--- events/api/models.py
+++ events/api/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 
 
+class Presenter(models.Model):
+    name = models.CharField(max_length=32)
+    surname = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self.name + " " + self.surname
+
+
 class Event(models.Model):
-    name = models.CharField(max_length=50)
+    title = models.CharField(max_length=50)
     location = models.CharField(max_length=50)
     date = models.DateField()
     mandatory = models.BooleanField(default=False)
+    presenter = models.ForeignKey(Presenter, on_delete=models.PROTECT, blank=True, null=True)
 
     def __str__(self):
-        return self.name
+        return self.title
