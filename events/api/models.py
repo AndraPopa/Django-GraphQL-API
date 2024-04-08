c82c2d88d60dca83c4dd8d33bf13f9d2ed4cefdd
AndraPopa <93611644+AndraPopa@users.noreply.github.com>
2024-04-08 10:30:15 +0300

    representation of event by name

--- events/api/models.py
+++ events/api/models.py
@@ -6,3 +6,6 @@ class Event(models.Model):
     location = models.CharField(max_length=50)
     date = models.DateField()
     mandatory = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
