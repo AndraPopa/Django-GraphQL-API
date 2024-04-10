c11a6123772532ddef19d5d38eeefbb06531ad70
AndraPopa <93611644+AndraPopa@users.noreply.github.com>
2024-04-10 12:13:20 +0300

    get event by query params

--- events/api/schema.py
+++ events/api/schema.py
@@ -10,6 +10,19 @@ class EventType(DjangoObjectType):
 
 class Query(graphene.ObjectType):
     all_events = graphene.List(EventType)
+    event = graphene.Field(EventType, id=graphene.Int(), title=graphene.String())
 
     def resolve_all_events(self, info, **kwargs):
         return Event.objects.all()
+
+    def resolve_event(self, info, **kwargs):
+        id = kwargs.get('id')
+        title = kwargs.get('title')
+
+        if id is not None:
+            return Event.objects.get(pk=id)
+
+        if title is not None:
+            return Event.objects.get(title=title)
+
+        return None
