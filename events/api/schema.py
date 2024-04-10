3888f4a184b5491238f6c88a9defe8510efe970b
AndraPopa <93611644+AndraPopa@users.noreply.github.com>
2024-04-10 12:24:47 +0300

    added custom query param for resolving days until an event

--- events/api/schema.py
+++ events/api/schema.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import graphene
 from graphene_django.types import DjangoObjectType
 from .models import Event
@@ -6,6 +8,19 @@ from .models import Event
 class EventType(DjangoObjectType):
     class Meta:
         model = Event
+
+    days_until = graphene.String()
+
+    def resolve_days_until(self, info):
+        current_date = datetime.now().date()
+        delta = self.date - current_date
+        days_until_event = delta.days
+        if days_until_event <= 3:
+            return "A little bit"
+        elif days_until_event < 0:
+            return "Already passed"
+        else:
+            return "A little bit more"
 
 
 class Query(graphene.ObjectType):
